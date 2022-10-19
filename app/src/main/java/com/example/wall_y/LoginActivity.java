83ff03ef23c1e0f99c8bc5535710fcb7eb261ecb
nessasunjaya <vanessasunjaya25@gmail.com>
2022-10-19 15:24:49 +0700

    added new User class, Login and Register reworked

--- app/src/main/java/com/example/wall_y/LoginActivity.java
+++ app/src/main/java/com/example/wall_y/LoginActivity.java
@@ -2,13 +2,30 @@ package com.example.wall_y;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView toRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        toRegister = (TextView) findViewById(R.id.toRegister);
+        toRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.toRegister:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/wall_y/RegisterActivity.java
+++ app/src/main/java/com/example/wall_y/RegisterActivity.java
@@ -1,14 +1,136 @@
 package com.example.wall_y;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView toLogin, title, titleshadow, subtitle;
+    private EditText email, password, confPassword;
+    private Button register;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        title = (TextView) findViewById(R.id.title);
+        title.setOnClickListener(this);
+
+        register = (Button) findViewById(R.id.register);
+        register.setOnClickListener(this);
+
+        toLogin = (TextView) findViewById(R.id.toLogin);
+        toLogin.setOnClickListener(this);
+
+        email = (EditText) findViewById(R.id.email);
+        password = (EditText) findViewById(R.id.password);
+        confPassword = (EditText) findViewById(R.id.confPassword);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.toLogin:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.title:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.register:
+                registerUser();
+                break;
+        }
+    }
+
+    private void registerUser(){
+        String newEmail = email.getText().toString().trim();
+        String newPassword = password.getText().toString().trim();
+        String newConfPassword = confPassword.getText().toString().trim();
+
+        if (newEmail.isEmpty()){
+            email.setError("Email is required!");
+            email.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(newEmail).matches()){
+            email.setError("Please provide valid email");
+            email.requestFocus();
+            return;
+        }
+        if(newPassword.isEmpty()){
+            password.setError("Password is Required!");
+            password.requestFocus();
+            return;
+        }
+        if(newConfPassword.isEmpty()){
+            confPassword.setError("Confirmation Password is required!");
+            confPassword.requestFocus();
+            return;
+        }
+
+        if(newPassword.length()<6){
+            password.setError("Password must be at least 6 characters!");
+            password.requestFocus();
+        }
+
+        if(!newPassword.equals(newConfPassword)){
+            password.setError("Password must be the same with Confirmation Password!");
+            password.requestFocus();
+
+            confPassword.setError("Confirmation Password must be the same with Password!");
+            confPassword.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(newEmail, newPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            User user = new User(newEmail, newPassword);
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if(task.isSuccessful()){
+                                                Toast.makeText(RegisterActivity.this, "User has been registered successfully!", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.VISIBLE);
+                                            }else{
+                                                Toast.makeText(RegisterActivity.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        }
+                                    });
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Failed to register! Try again!", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
     }
 }
